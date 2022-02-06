954fceb77af34cc27c2f247639e568ada8d1fb12
Raghav020401 <86782391+Rag-lag@users.noreply.github.com>
2022-02-06 12:14:50 +0300

    ID and Secret changes

--- app.js
+++ app.js
@@ -73,8 +73,8 @@ passport.deserializeUser(function (user, cb) {
 });
 
 passport.use(new GoogleStrategy({
-        clientID: process.env.CLIENT_ID,
-        clientSecret: process.env.CLIENT_SECRET,
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         callbackURL: "http://localhost:3000/auth/google/secrets",
         scope: 'profile'
     },
